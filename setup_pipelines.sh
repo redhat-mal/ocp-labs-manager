0877803821f1ad8ac17d91d2ba2566f519a3eb14
Mike LaCourse <mlacours@localhost.localdomain>
2021-02-03 15:00:31 -0500

    fix params

--- setup_pipelines.sh
+++ setup_pipelines.sh
@@ -25,8 +25,10 @@ echo "Configuring pipelines"
 SONAR_ROUTE=$(oc get route sonarqube-insecure --no-headers=true | awk '{ print $2 }')
 echo $SONAR_ROUTE
 
-if [[ -z "$MY_GOOGLECHAT_TOKEN" ]]; then
-helm template pipelines helm/argo-applications/ --set googlechat_secret=$MY_GOOGLECHAT_SECRET --set googlechat_channel=$MY_GOOGLECHAT_CHANNEL --set googlechat_token=$MY_GOOGLECHAT_TOKEN --set googlechat_key=$MY_GOOGLECHAT_KEY --set sonar_route=http://$SONAR_ROUTE  --set github_secret_username=$MY_GIT_USERNAME  --set github_secret_username=$MY_GIT_USERNAME  | oc apply -f-
-else
+if [[ -z "$MY_GOOGLECHAT_SECRET" ]]; then
+echo "Chat Disabled"
 helm template pipelines helm/argo-applications/ --set github_secret_username=$MY_GIT_USERNAME --set github_secret_token=$MY_GIT_TOKEN --set sonar_route=http://$SONAR_ROUTE | oc apply -f-
+else
+echo "Chat Enabled"
+helm template pipelines helm/argo-applications/ --set github_secret_username=$MY_GIT_USERNAME --set github_secret_token=$MY_GIT_TOKEN --set googlechat_secret=$MY_GOOGLECHAT_SECRET --set sonar_route=http://$SONAR_ROUTE | oc apply -f-
 fi
