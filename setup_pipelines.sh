66b54f398397c485bc723d642ecdec842b7fd0c5
Mike LaCourse <mlacours@localhost.localdomain>
2021-02-04 12:36:51 -0500

    refactor app update

--- setup_pipelines.sh
+++ setup_pipelines.sh
@@ -19,7 +19,7 @@ fi
 sleep 60s
 echo "Installing Pipeline" 
 oc project cicd-tools
-helm install cicd-tools ./helm/lab-pipeline-tools/pipeline-tools/
+helm upgrade --install cicd-tools ./helm/lab-pipeline-tools/pipeline-tools/
 
 echo "Configuring pipelines"
 SONAR_ROUTE=$(oc get route sonarqube-insecure --no-headers=true | awk '{ print $2 }')
@@ -32,3 +32,4 @@ else
 echo "Chat Enabled"
 helm template pipelines helm/argo-applications/ --set github_secret_username=$MY_GIT_USERNAME --set github_secret_token=$MY_GIT_TOKEN --set googlechat_secret=$MY_GOOGLECHAT_SECRET --set sonar_route=http://$SONAR_ROUTE | oc apply -f-
 fi
+
